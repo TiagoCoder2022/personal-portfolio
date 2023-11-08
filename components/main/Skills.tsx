b3a691d1f0bf89a21d5453f5825a33db78072106
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-11-08 13:28:29 -0300

    feat: add TitleSection componet

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -3,13 +3,13 @@
 import React from "react";
 import { Frontend_skill } from "@/constants";
 import SkillsCard from "../sub/SkillsCard";
+import TitleSection from "./TitleSection";
+import { slideInFromRight } from "@/utils/motion";
 
 const Skills = () => {
   return (
     <section className="container mx-auto flex flex-col items-center justify-center">
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-        My Skills
-      </h1>
+      <TitleSection variants={slideInFromRight(0.5)}>My Stack</TitleSection>
       <div className="mt-20 justify-center flex flex-wrap gap-10">
         {Frontend_skill.map((image, index) => (
           <SkillsCard
