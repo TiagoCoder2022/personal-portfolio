a8ab98ebae0bb40ba2ac08d817e2a5c2c893c0b8
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-11-08 13:26:05 -0300

    feat: create About section component

---
+++ components/main/About.tsx
@@ -0,0 +1,105 @@
+"use client";
+import Image from "next/image";
+import React, { useState, useTransition } from "react";
+import TabButton from "./TabButton";
+import TitleSection from "./TitleSection";
+import { slideInFromRight, slideInFromTop } from "@/utils/motion";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul>
+        <li>React</li>
+        <li>JavaScript</li>
+        <li>Node.js</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Tailwind</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul>
+        <li>Gestao Tecnologia da Informaçao</li>
+        <li>Universidade Paulista - UNIP</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Courses",
+    id: "courses",
+    content: (
+      <ul>
+        <li>Full Stack Javascript - OneBitCode</li>
+        <li>Front-end Developer - Origamid</li>
+      </ul>
+    ),
+  },
+];
+
+const About = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id: any) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+  return (
+    <section className="container mx-auto flex flex-col items-center justify-center z-[20]">
+      <TitleSection variants={slideInFromRight(0.8)}>About Me</TitleSection>
+
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-14 sm:py-16 xl:px-16">
+        <Image
+          src="/tiago-profile.jpg"
+          width={400}
+          height={400}
+          alt="Profile picture"
+          className="object-contain rounded-lg shadow-card"
+        />
+        <div className="text-white mt-4 md:mt-0 text-left flex flex-col h-full">
+          <p className="text-base lg:text-lg">
+            I am a Full Stack web developer with a passion for creating
+            interactive and responsive web aplications. I have experience
+            working with javascript, React, Redux, Node.js, Express, PostgreSQL,
+            HTML, CSS, and Git. I am a quick learner and I am always looking to
+            expand my knowledge and skill set. I am a team player and I am
+            excited to work with others to create amazing applications.
+          </p>
+
+          <div className="flex flex-row mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              Education
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("courses")}
+              active={tab === "courses"}
+            >
+              Courses
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab)?.content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
