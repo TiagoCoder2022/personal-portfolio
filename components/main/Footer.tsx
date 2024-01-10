8d204e135cfda1ca63cf850df4189c06f0aeaff0
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-01-09 22:13:05 -0300

    feat: add our footer and Projectcard

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -6,7 +6,9 @@ const Footer = () => {
     <footer className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px]">
       <div className="w-full flex flex-col items-center justify-center m-auto">
         <div className="w-full h-full flex flex-col items-center justify-around flex-wrap">
-          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start"></div>
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+            <h2>Footer</h2>
+          </div>
         </div>
       </div>
     </footer>
