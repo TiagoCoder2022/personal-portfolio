0d7e1fc6a06fcf607aa3fd43faeb21d842741650
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-01-09 22:11:31 -0300

    feat: setting new font to the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import About from "@/components/main/About";
-import Hero from "@/components/main/Hero";
+import Contact from "@/components/main/Contact";
+import Footer from "@/components/main/Footer";
+import HomeS from "@/components/main/Home";
 import Projects from "@/components/main/Projects";
-import Skills from "@/components/main/Skills";
+import Skills from "@/components/sub/Skills";
 
 export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
-        <Hero />
+        <HomeS />
         <About />
         <Skills />
         <Projects />
+        <Contact />
+        <Footer />
       </div>
     </main>
   );
