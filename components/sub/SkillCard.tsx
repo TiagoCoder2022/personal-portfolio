78e9464c8ae19ccc1920c8287c536f1a64a27ad2
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-01-09 22:06:01 -0300

    feat: add Skill and SkilCard component

---
+++ components/sub/SkillCard.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Tilt from "react-parallax-tilt";
+import React from "react";
+import { IconType } from "react-icons/lib";
+import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs } from "react-icons/fa";
+import { SiTailwindcss, SiTypescript, SiRedux } from "react-icons/si";
+
+interface SkillCardProps {
+  id: number;
+  name: string;
+  title: string;
+}
+
+const getIconForSkill = (skillName: string): IconType | null => {
+  switch (skillName) {
+    case "html":
+      return FaHtml5;
+    case "css":
+      return FaCss3;
+    case "javascript":
+      return FaJs;
+    case "react":
+      return FaReact;
+    case "tailwind":
+      return SiTailwindcss;
+    case "redux":
+      return SiRedux;
+    case "typescript":
+      return SiTypescript;
+    case "node":
+      return FaNodeJs;
+    default:
+      return null;
+  }
+};
+
+const SkillCard: React.FC<SkillCardProps> = ({ id, name, title }) => {
+  const IconComponent = getIconForSkill(name);
+
+  if (!IconComponent) {
+    // Handle case where the skill name doesn't match any known icon
+    return null;
+  }
+
+  return (
+    <Tilt
+      tiltMaxAngleX={20}
+      tiltMaxAngleY={20}
+      transitionSpeed={1500}
+      scale={1.02}
+      gyroscope={true}
+      className=" green-pink-gradient w-48 p-[1px] rounded-[20px] shadow-card z-[20]"
+    >
+      <motion.div>
+        <div className="bg-tertiary rounded-[20px] py-5 px-12 w-[192px] min-h-[192px] flex justify-evenly items-center flex-col">
+          <IconComponent size={50} fill="#00e5e5" />
+
+          <h3 className="text-white text-[15px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+export default SkillCard;
---
+++ components/sub/Skills.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import SkillsCardTest from "./SkillCard";
+import TitleSection from "../main/TitleSection";
+import { slideInFromRight } from "@/utils/motion";
+import { skill_frontend } from "@/constants";
+import SkillCard from "./SkillCard";
+
+const Skills = () => {
+  return (
+    <section
+      id="skills"
+      className="container max-w-4xl mx-auto flex flex-col items-center justify-center"
+    >
+      <TitleSection variants={slideInFromRight(0.5)}>My Stack</TitleSection>
+      <div className="mt-20 justify-center flex flex-wrap gap-10">
+        {skill_frontend.map((skill) => (
+          <SkillCard key={skill.id} {...skill} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
