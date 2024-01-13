d8f8c3502a31b05e80addf2f77c4ca91079c599c
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-01-13 17:56:54 -0300

    feat: add new bg and refact the content responsivity

--- components/main/HeroSection.tsx
+++ components/main/HeroSection.tsx
@@ -12,38 +12,35 @@ const HeroSection = () => {
   return (
     <section
       id="home"
-      className="container flex flex-col items-center justify-center mx-auto lg:mx-16 max-w-[1920px] z-20"
+      className="flex flex-col h-[100vh] mx-auto md:container max-w-[1920px] z-20"
     >
-      <div className="w-full relative">
-        <div
-          className=" absolute inset-0 h-full w-full object-cover"
-          style={{
-            backgroundImage: "url('/lost-in-space.jpg')",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        ></div>
-        <div className="absolute inset-0 bg-gradient-to-l from-transparent to-[#030014]/100" />
+      <div
+        className="absolute inset-0 h-full w-full object-cover"
+        style={{
+          backgroundImage: "url('/contact-bg.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></div>
 
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#030014]/100" />
+
+      <div className="w-full relative py-[45%] md:py-[20%] lg:py-[15%]">
         <motion.div
           initial="hidden"
           animate="visible"
-          className="flex py-[40%] md:py-[20%] lg:py-[12%] flex-col px-10 md:px-14 min-h-screen w-full"
+          className="flex flex-col w-full"
         >
-          <div className="absolute h-full w-full  flex flex-col">
+          <div className="h-full w-full  flex flex-col">
             <motion.div
               variants={slideInFromLeft(0.5)}
               className="flex flex-col font-bold text-white w-auto lg:h-auto"
             >
-              <span className="text-3xl md:text-4xl lg:text-4xl xl:text-5xl">
+              <span className="text-3xl md:text-4xl lg:text-4xl xl:text-6xl">
                 HELLO,
                 <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
                   {" "}
-
                   I&apos;m TIAGO{" "}
-
-           
-
                 </span>
                 <br></br>
                 <span className="text-4xl md:text-4xl lg:text-5xl xl:text-6xl">
@@ -68,14 +65,14 @@ const HeroSection = () => {
 
             <motion.p
               variants={slideInFromLeft(0.8)}
-              className="lg:text-sm xl:text-lg text-center md:text-start mt-10 max-w-xs  lg:max-w-md xl:max-w-lg text-[#ADB7BE] text-xs"
+              className="lg:text-sm xl:text-xl text-center md:text-start mt-10 max-w-xs  lg:max-w-md xl:max-w-xl text-[#ADB7BE] text-sm"
             >
               {" "}
               I&apos;m a Full Stack Software Developer with experience in
               website, Check out my projects and skills.
             </motion.p>
 
-            <div className="flex justify-start gap-3 md:gap-4 text-xs md:text-sm py-4">
+            <div className="flex justify-start gap-4 md:gap-6 text-xs md:text-sm py-4">
               <motion.a
                 variants={slideInFromRight(1)}
                 className="py-3 px-6 rounded-sm w-fit button-primary text-center text-white cursor-pointer button-scyfi"
