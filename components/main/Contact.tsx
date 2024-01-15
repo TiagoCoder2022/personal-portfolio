fc16b26adccbce524fb7575912393c1fdc8c0901
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-01-15 12:01:34 -0300

    feat: add motion to Contact section

--- components/main/Contact.tsx
+++ components/main/Contact.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
+"use client";
 import React from "react";
 import { motion } from "framer-motion";
-import { fadeIn, slideInFromRight } from "@/utils/motion";
 import TitleSection from "../main/TitleSection";
 import { SparklesIcon } from "@heroicons/react/24/solid";
 
@@ -11,34 +10,41 @@ const Contact = () => {
       id="contact"
       className="flex flex-col items-center justify-center max-w-[1920px]"
     >
-      <TitleSection variants={slideInFromRight(0.5)}>Contact</TitleSection>
-      {/*<div className=" relative z-[-1] w-[100vw] h-[100vh] hidden md:block">
-        <Image
-          unoptimized
-          src="/contact-bg.jpg"
-          layout="fill"
-          alt="picture"
-          objectFit="cover"
-        />
-  </div>*/}
+      <TitleSection>Contact</TitleSection>
 
       <div className="container mx-auto px-4">
         <div className="flex flex-col lg:flex-row">
           <div className="flex-1">
-            <div>
-              <div className="flex flex-row gap-2">
-                <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-                <h4 className="text-xl uppercase text-cyan-500 font-medium mb-b tracking-wide">
-                  Get in touch
-                </h4>
-              </div>
+            <motion.div
+              initial={{ opacity: 0, x: -100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1, delay: 0.1 }}
+              className="flex flex-row gap-2"
+            >
+              <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+              <h4 className="text-xl uppercase text-cyan-500 font-medium mb-b tracking-wide">
+                Get in touch
+              </h4>
+            </motion.div>
 
-              <h3 className="text-[45px] uppercase lg:text-[70px] leading-none mb-12 text-white ">
-                LET&apos;S WORK <br /> TOGETHER!
-              </h3>
-            </div>
+            <motion.h3
+              initial={{ opacity: 0, x: -100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1, delay: 0.7 }}
+              className="text-[45px] uppercase lg:text-[70px] leading-none mb-12 text-white "
+            >
+              LET&apos;S WORK <br /> TOGETHER!
+            </motion.h3>
           </div>
-          <form className="flex-1 border flex flex-col rounded-md gap-y-6 pb-24 p-6 w-full items-start max-w-xl">
+          <motion.form
+            initial={{ opacity: 0, x: 100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -100 }}
+            transition={{ duration: 1, delay: 0.1 }}
+            className="flex-1 border flex flex-col rounded-md gap-y-6 pb-24 p-6 w-full items-start max-w-xl"
+          >
             <input
               className="bg-transparent border-b py-3 outline-none w-full text-white placeholder:text-cyan-400 focus:border-purple-500 transition-al"
               type="text"
@@ -56,7 +62,7 @@ const Contact = () => {
             <a className="py-3 px-6 w-fit button-primary rounded-sm text-center text-white cursor-pointer button-scyfi">
               Send message
             </a>
-          </form>
+          </motion.form>
         </div>
       </div>
     </section>
