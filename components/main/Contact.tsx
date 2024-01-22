b4ae1e53b579d8a358e90d525992eec90e1cbac3
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-01-22 13:34:29 -0300

    feat: add a email send functionality on contact form

--- components/main/Contact.tsx
+++ components/main/Contact.tsx
@@ -1,10 +1,44 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import TitleSection from "../sub/TitleSection";
 import { SparklesIcon } from "@heroicons/react/24/solid";
+import { sendEmail } from "@/sendEmail/sendEmail";
 
 const Contact = () => {
+  const [emailSubmitted, setEmailSubmitted] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.email.value,
+      subject: e.target.subject.value,
+      message: e.target.message.value,
+    };
+
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "/api/send";
+
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: "POST",
+      // Tell the server we're sending JSON.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    };
+
+    const response = await fetch(endpoint, options);
+    const resData = await response.json();
+
+    if (response.status === 200) {
+      console.log("Message sent.");
+      setEmailSubmitted(true);
+    }
+  };
   return (
     <section
       id="contact"
@@ -43,25 +77,38 @@ const Contact = () => {
             whileInView={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: -100 }}
             transition={{ duration: 1, delay: 0.1 }}
+            action={async (formData) => {
+              await sendEmail(formData);
+            }}
             className="flex-1 border flex flex-col rounded-md gap-y-6 pb-16 p-6 w-full items-start max-w-xl backdrop-blur-md"
           >
             <input
               className="bg-transparent border-b py-3 outline-none w-full text-white placeholder:text-cyan-400 focus:border-purple-500 transition-al"
-              type="text"
-              placeholder="Your name"
+              type="email"
+              name="senderEmail"
+              maxLength={500}
+              required
+              placeholder="Your email"
             />
             <input
               className="bg-transparent border-b py-3 outline-none w-full text-white placeholder:text-cyan-400 focus:border-purple-500 transition-all"
               type="text"
-              placeholder="Your email"
+              name="subject"
+              placeholder="Subject"
             />
             <textarea
               className="bg-transparent border-b py-12 outline-none w-full text-white placeholder:text-cyan-400 resize-none mb-12 focus:border-purple-500 transition-all"
               placeholder="Your message"
+              name="message"
+              required
+              maxLength={5000}
             ></textarea>
-            <a className="py-3 px-6 w-fit button-primary rounded-sm text-center text-white cursor-pointer button-scyfi">
+            <button
+              type="submit"
+              className="py-3 px-6 w-fit button-primary rounded-sm text-center text-white cursor-pointer button-scyfi"
+            >
               Send message
-            </a>
+            </button>
           </motion.form>
         </div>
       </div>
